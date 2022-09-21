508cae6ddc08fb15da516368bd883cbe1aeea1fb
Kendi-B <Kendi.mwenda95@gmail.com>
2022-09-21 11:22:48 -0700

    Hello printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-int main(void) 
+int main(void) //This is the main function
+
 {
-    char MyStr[] = "Programming is like building a multilingual puzzle";
-    puts(MyStr);	
-    return (0);
+       	char MyStr[] = ""Programming is like building a multilingual puzzle";
+        puts(MyStr);	
+        return (0);
 }
