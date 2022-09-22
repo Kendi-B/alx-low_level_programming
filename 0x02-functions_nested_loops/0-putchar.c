6cf80b65e0544766119b1ddea2c2c9d448cfc467
Kendi-B <Kendi.mwenda95@gmail.com>
2022-09-22 12:23:39 -0700

    putschar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main- Main function the entry point  of the program 
- * _putchar writes the caharacter to stdout
- * Return zero if not successful
+ *main-Main function the entry point of the program 
+ *_putchar writes the caharacter to stdout
+ *Return zero if not successful
  */
 int main(void)
 {
