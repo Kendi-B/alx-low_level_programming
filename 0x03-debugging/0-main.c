d10487d388283386a30d9251d64c1b77a13721df
Kendi-B <Kendi.mwenda95@gmail.com>
2022-09-23 15:33:55 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests the function that outputs if integer is postive or negative
+ * Return: 0
+ */
+
+int maain(void)
+{
+	int i;
+
+	i=0;
+	positive_or_negative(i);
+
+	return (0);
+}
