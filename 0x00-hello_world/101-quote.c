dc630b5b56a076c09064046ecd2dfa8c80dd0fad
Kendi-B <Kendi.mwenda95@gmail.com>
2022-09-25 06:02:57 -0700

    You are a genius

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - starting point
+ * main - entry point
  *
- * Return: 1, There is some error
+ * Return: 1, prints to the  error
  */
-
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
